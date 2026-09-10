7223f307f230a20aa1bd6a9f4ac4af8804065d51
xuanpn <111854246+xuanpn@users.noreply.github.com>
2026-09-10 08:55:24 +0700

    edit 10092026

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
    Xem hướng dẫn lấy 2 giá trị này trong README (mục Supabase).
    ============================================================ */
 window.SUPABASE = {
-    url: "https://swwosluqkrjojarodroi.supabase.co/rest/v1/"",            // VD: "https://swwosluqkrjojarodroi.supabase.co/rest/v1/"
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3d29zbHVxa3Jqb2phcm9kcm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5ODYzNjAsImV4cCI6MjEwNDU2MjM2MH0.f_AYb9h8waH4Me0SQH1c44GTCgznnBIlr7Hg3XX4VLI",        // VD: "eyJhbGciOi...."  (khoá "anon public", KHÔNG dùng service_role)
+  url: "https://swwosluqkrjojarodroi.supabase.co/rest/v1/",            // VD: "https://abcdefghijkl.supabase.co"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3d29zbHVxa3Jqb2phcm9kcm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5ODYzNjAsImV4cCI6MjEwNDU2MjM2MH0.f_AYb9h8waH4Me0SQH1c44GTCgznnBIlr7Hg3XX4VLI",        // VD: "eyJhbGciOi...."  (khoá "anon public", KHÔNG dùng service_role)
   table: "results",   // giữ nguyên nếu bạn chạy đúng câu SQL trong README
   event: "hanoi-2026" // đổi thành "haiphong-2026" nếu dùng lại web cho khu vực khác
 };
